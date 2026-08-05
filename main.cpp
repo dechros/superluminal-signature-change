96d71e63e6fa878c255d79e0526aa2700821280f
Halit Cetin <halitcetin@live.com>
2026-08-05 07:52:19 +0300

    Kod cekirdege indirildi: 90 modulden 41'e
    Cekirdek tanimi: protonun superluminal bolgeye gecmesi, orada MEKANDA yer
    degistirmesi, geri donmesi ve zamanda yer degistirdiginin gozlenmesi. Matematik,
    fizik, deney. Buna hizmet etmeyen her sey cikarildi.
    
    Cikanlar (54 modul): kara delik karsilastirmasi ve ufuk katmanlari, kuantumlama
    (alti modul), literatur itirazlari ve uzlastirma apparati, kuresel cozum, sinyal
    kanallari, genisletilmis izometri grubu, durgunluk yorungesi, tarama modullerinin
    cogu, ve cekirdek zincirinde yeri olmayan parcacik modulleri.
    
    Kalanlar (41 modul): iki bolge ve gecis haritasi, ara bolge ve iki gecisin
    genligi, dilim gecirgenligi ve eklem eslesmesi, uc okuma (nokta cisim, dalga,
    genlik), proton paketi ve gidis donus defteri, ote taraf sozlugu ve MEKANIN OKU
    (yon denetimi, ote taraf hareketi, cikis yuzu), donus olayi ve kapali formu,
    paket simulasyonu, kabul kosullari, laboratuvar birimleri ve deney belirtimi,
    ve metni koruyan uc arac.
    
    Dort modul silindikten sonra derleyici tarafindan geri cagrildi, cunku cekirdegi
    tasiyorlarmis: ProfiledBarrier (yon icin karsiliklilik sinamasi), AsymmetricFaces,
    ThresholdOptimum (bedelin en kucuk degeri), PermutationScan.
    
    Kullanicinin son duzeltmesi de kayda gecti: ote tarafta ZAMANIN oku degil
    MEKANIN oku onemli, cunku buradaki zamani belirleyen sey oradaki uzamsal eksenin
    yonu.
    
    824 denetim yesil, sifir uyari.

--- main.cpp
+++ main.cpp
@@ -1,86 +1,32 @@
 #include "admission/CrossingAdmission.h"
-#include "boundary/CausalCrossing.h"
 #include "boundary/JunctionFamily.h"
 #include "boundary/JunctionScattering.h"
-#include "boundary/ModeFilter.h"
-#include "boundary/ProfiledBarrier.h"
 #include "boundary/SlabTunnelling.h"
-#include "boundary/ThresholdSurface.h"
 #include "charge/ChargedRoundTrip.h"
 #include "core/Report.h"
 #include "core/Section.h"
-#include "critique/AlternativeRoutes.h"
 #include "critique/ArticleSync.h"
-#include "critique/CommonFormulas.h"
-#include "critique/LiteratureObjections.h"
-#include "critique/ObjectionTests.h"
-#include "critique/OpenQuestions.h"
 #include "critique/ProseRegister.h"
-#include "critique/Reconciliation.h"
-#include "critique/ReproductionLedger.h"
-#include "critique/SignatureObstruction.h"
 #include "dynamics/Dispersion.h"
-#include "dynamics/EnergyMomentum.h"
-#include "field/DynamicsUnderReversal.h"
-#include "field/ScalarWaveField.h"
-#include "field/VectorGaugeField.h"
-#include "field/ReversalSymmetry.h"
 #include "field/TimeOrientation.h"
 #include "identity/RoundTripInvariants.h"
-#include "horizon/BlackHoleHorizon.h"
-#include "horizon/DegeneratePath.h"
-#include "horizon/LayerEnergyConditions.h"
-#include "horizon/RateCondition.h"
-#include "horizon/TransversePrescription.h"
 #include "horizon/SurfaceLayer.h"
-#include "intermediate/ChannelEntropy.h"
 #include "intermediate/DwellTime.h"
 #include "intermediate/IntermediateRegion.h"
-#include "intermediate/SignalFront.h"
-#include "intermediate/ThresholdOptimum.h"
 #include "intermediate/TraversalClocks.h"
 #include "intermediate/TwoCrossings.h"
-#include "transform/ExtendedIsometryGroup.h"
-#include "particle/ArrivalOrder.h"
-#include "particle/AsymmetricFaces.h"
-#include "particle/CellDistribution.h"
 #include "particle/DirectionControl.h"
-#include "particle/EnergyBookkeeping.h"
 #include "particle/ExitFace.h"
 #include "particle/FarSideMotion.h"
-#include "particle/FarSideObservation.h"
-#include "particle/Feasibility.h"
-#include "particle/FlowAssumption.h"
-#include "particle/StateTable.h"
-#include "particle/LoopClosure.h"
 #include "particle/ReturnEvent.h"
 #include "particle/ReturnFormula.h"
 #include "particle/RoundTrip.h"
-#include "particle/TimeHolonomy.h"
 #include "particle/TimeProjection.h"
-#include "particle/TimeReversalSignature.h"
 #include "particle/WavePacket.h"
 #include "particle/WorkedRoundTrip.h"
-#include "quantum/CanonicalQuantization.h"
-#include "quantum/QuadraticInvariant.h"
-#include "quantum/DerivedTransition.h"
-#include "quantum/LapseContour.h"
-#include "quantum/PathIntegral.h"
-#include "quantum/BoundaryVacuumEnergy.h"
 #include "routes/ThreeRoutes.h"
-#include "rest/RestStateOrbit.h"
-#include "scan/BranchObservables.h"
-#include "scan/CrossingBranches.h"
-#include "scan/PermutationScan.h"
-#include "scan/SlotDictionary.h"
-#include "signal/Channels.h"
 #include "sim/PacketSimulation.h"
 #include "critique/AssumptionLedger.h"
-#include "dynamics/ConjugateDictionary.h"
-#include "particle/RouteHolonomy.h"
-#include "particle/TimeShiftCost.h"
-#include "signal/GravitationalChannel.h"
-#include "spherical/SphericalSolution.h"
 #include "transform/SignatureInvolution.h"
 #include "units/PhysicalScales.h"
 #include "experiment/RoundTripExperiment.h"
@@ -99,74 +45,25 @@ namespace
     {
         std::vector<std::unique_ptr<slm::Section>> sections;
         sections.push_back(std::make_unique<slm::SignatureInvolution>());
-        sections.push_back(std::make_unique<slm::PermutationScan>());
-        sections.push_back(std::make_unique<slm::CrossingBranchesSection>());
-        sections.push_back(std::make_unique<slm::BranchObservablesSection>());
-        sections.push_back(std::make_unique<slm::SlotDictionarySection>());
         sections.push_back(std::make_unique<slm::IntermediateRegionSection>());
-        sections.push_back(std::make_unique<slm::ThresholdSurface>());
         sections.push_back(std::make_unique<slm::JunctionScattering>());
         sections.push_back(std::make_unique<slm::JunctionFamilySection>());
         sections.push_back(std::make_unique<slm::SurfaceLayerSection>());
-        sections.push_back(std::make_unique<slm::LayerEnergyConditionsSection>());
-        sections.push_back(std::make_unique<slm::RateConditionSection>());
-        sections.push_back(std::make_unique<slm::TransversePrescriptionSection>());
-        sections.push_back(std::make_unique<slm::CausalCrossingSection>());
         sections.push_back(std::make_unique<slm::SlabTunnellingSection>());
-        sections.push_back(std::make_unique<slm::ProfiledBarrierSection>());
-        sections.push_back(std::make_unique<slm::DegeneratePathSection>());
-        sections.push_back(std::make_unique<slm::BlackHoleHorizon>());
-        sections.push_back(std::make_unique<slm::ScalarWaveField>());
-        sections.push_back(std::make_unique<slm::EnergyMomentum>());
         sections.push_back(std::make_unique<slm::TimeOrientationSection>());
-        sections.push_back(std::make_unique<slm::ReversalSymmetrySection>());
-        sections.push_back(std::make_unique<slm::DynamicsUnderReversalSection>());
-        sections.push_back(std::make_unique<slm::RestStateOrbit>());
         sections.push_back(std::make_unique<slm::DispersionSection>());
-        sections.push_back(std::make_unique<slm::ConjugateDictionarySection>());
-        sections.push_back(std::make_unique<slm::RouteHolonomySection>());
-        sections.push_back(std::make_unique<slm::TimeShiftCostSection>());
         sections.push_back(std::make_unique<slm::AssumptionLedgerSection>());
         sections.push_back(std::make_unique<slm::ProseRegisterSection>());
         sections.push_back(std::make_unique<slm::ArticleSyncSection>());
-        sections.push_back(std::make_unique<slm::SphericalSolution>());
-        sections.push_back(std::make_unique<slm::CanonicalQuantization>());
-        sections.push_back(std::make_unique<slm::PathIntegral>());
-        sections.push_back(std::make_unique<slm::LapseContourSection>());
-        sections.push_back(std::make_unique<slm::DerivedTransitionSection>());
-        sections.push_back(std::make_unique<slm::BoundaryVacuumEnergy>());
-        sections.push_back(std::make_unique<slm::QuadraticInvariantSection>());
         sections.push_back(std::make_unique<slm::WavePacketSection>());
-        sections.push_back(std::make_unique<slm::ModeFilterSection>());
         sections.push_back(std::make_unique<slm::TwoCrossingsSection>());
         sections.push_back(std::make_unique<slm::DwellTimeSection>());
-        sections.push_back(std::make_unique<slm::ChannelEntropySection>());
         sections.push_back(std::make_unique<slm::TraversalClocksSection>());
-        sections.push_back(std::make_unique<slm::ThresholdOptimumSection>());
-        sections.push_back(std::make_unique<slm::SignalFrontSection>());
         sections.push_back(std::make_unique<slm::RoundTripSection>());
-        sections.push_back(std::make_unique<slm::EnergyBookkeepingSection>());
-        sections.push_back(std::make_unique<slm::StateTableSection>());
         sections.push_back(std::make_unique<slm::ExitFaceSection>());
-        sections.push_back(std::make_unique<slm::CellDistributionSection>());
-        sections.push_back(std::make_unique<slm::AsymmetricFacesSection>());
-        sections.push_back(std::make_unique<slm::ExtendedIsometryGroup>());
-        sections.push_back(std::make_unique<slm::ChannelsSection>());
-        sections.push_back(std::make_unique<slm::GravitationalChannelSection>());
-        sections.push_back(std::make_unique<slm::VectorGaugeFieldSection>());
-        sections.push_back(std::make_unique<slm::SignatureObstruction>());
-        sections.push_back(std::make_unique<slm::ObjectionTestsSection>());
-        sections.push_back(std::make_unique<slm::LiteratureObjectionsSection>());
-        sections.push_back(std::make_unique<slm::AlternativeRoutesSection>());
-        sections.push_back(std::make_unique<slm::CommonFormulasSection>());
-        sections.push_back(std::make_unique<slm::ReconciliationSection>());
-        sections.push_back(std::make_unique<slm::OpenQuestionsSection>());
-        sections.push_back(std::make_unique<slm::ReproductionLedgerSection>());
         sections.push_back(std::make_unique<slm::FarSideMotionSection>());
         sections.push_back(std::make_unique<slm::DirectionControlSection>());
-        sections.push_back(std::make_unique<slm::FarSideObservationSection>());
         sections.push_back(std::make_unique<slm::WorkedRoundTripSection>());
-        sections.push_back(std::make_unique<slm::FeasibilitySection>());
         sections.push_back(std::make_unique<slm::PacketSimulationSection>());
         sections.push_back(std::make_unique<slm::RoundTripInvariantsSection>());
         sections.push_back(std::make_unique<slm::ThreeRoutesSection>());
@@ -178,13 +75,8 @@ namespace
         sections.push_back(std::make_unique<slm::ThresholdScalingSection>());
         sections.push_back(std::make_unique<slm::RoundTripExperimentSection>());
         sections.push_back(std::make_unique<slm::TimeProjectionSection>());
-        sections.push_back(std::make_unique<slm::FlowAssumptionSection>());
-        sections.push_back(std::make_unique<slm::TimeReversalSignatureSection>());
-        sections.push_back(std::make_unique<slm::TimeHolonomySection>());
         sections.push_back(std::make_unique<slm::ReturnEventSection>());
         sections.push_back(std::make_unique<slm::ReturnFormulaSection>());
-        sections.push_back(std::make_unique<slm::ArrivalOrderSection>());
-        sections.push_back(std::make_unique<slm::LoopClosureSection>());
         return sections;
     }
 
